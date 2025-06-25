0c2ca24078b67be357a4ecceabdb8916dfd55e34
Alex <aleksandrseryakov@MacBook-Air.local>
2025-06-25 19:33:04 +0300

    Управление задачами, рабочая версия кода

---
+++ task_management.py
@@ -0,0 +1,27 @@
+class TaskList:
+    def __init__(self):
+        self.__task_list = []
+
+    def __is_task_in_list(self, task):
+        for t in self.__task_list:
+            if t['name'] == task:
+                return True
+        return False  
+
+    def add_task(self, task):
+        if not self.__is_task_in_list(task):
+            self.__task_list.append({'name': task, 'done': False})
+            print(f'Задача "{task}" добавлена в списко')
+        else:
+            print(f'Задача "{task}" уже есть в списке')
+
+    def remove_task(self, task_name):
+        if self.__is_task_in_list(task_name):
+            self.__task_list.remove(task_name)
+            print(f'Задача "{task_name}" удалена из списка')
+        else:
+            print(f'Задачи "{task_name}" нет в списке')
+
+task_list = TaskList()
+task_list.add_task("Task 1")
+print(task_list._TaskList__task_list)
